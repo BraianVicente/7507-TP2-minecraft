18c7e5234f1e265d4470b6ae418ae09fbba58bc5
Joaquin Andresen <joaquin_andresen@icloud.com>
2019-06-01 11:46:31 -0300

    Elimino Foo y agrego archivos Herramienta y TestHerramienta

---
+++ src/fiuba/algo3/Herramientas/Herramienta.java
@@ -0,0 +1,5 @@
+package fiuba.algo3.Herramientas
+
+public class Herramienta {
+
+}
---
+++ test/TestHerramienta.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+public class TestHerramienta {
+
+    @Test
+    public void CreoHachaDeMaderaConDurabilidadYFuerza(){
+
+    }
+
+    @Test
+    public void CreoPicoDeMaderaConDurabilidadYFuerza(){
+
+    }
+
+    @Test
+    public void CreoHachaDeMetalConDurabilidadYFuerza(){
+
+    }
+
+    @Test
+    public void CreoPicoDeMetalConDurabilidadYFuerza(){
+
+    }
+}
