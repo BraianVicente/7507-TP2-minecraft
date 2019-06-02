b52e9eea2308736f0453d8d816042606f9ab87f2
lopezlin <morgan_pirata2003@yahoo.com.ar>
2019-06-01 23:25:07 -0300

    Se crea clase PicoDePiedra

---
+++ src/fiuba/algo3/Herramientas/PicoDePiedra.java
@@ -0,0 +1,13 @@
+package fiuba.algo3.Herramientas;
+
+public class PicoDePiedra extends Herramienta {
+
+    static int DURABILIDAD_PICO_PIEDRA = 200;
+    static int FUERZA_PICO_PIEDRA      = 4;
+
+    public PicoDePiedra ()
+        {
+            this.durabilidad = DURABILIDAD_PICO_PIEDRA;
+            this.fuerza = FUERZA_PICO_PIEDRA;
+        }
+}
