570fc3a2f5ee0edce2a6259091c23adb159ecdd6
Joaquin Andresen <joaquin_andresen@icloud.com>
2019-06-02 11:34:19 -0300

    Agrego clase Jugador

---
+++ src/fiuba/algo3/minecraft/jugador/Jugador.java
@@ -0,0 +1,14 @@
+package fiuba.algo3.minecraft.jugador;
+
+public class Jugador {
+
+    public String nombre;
+
+    public Jugador(String nombre){
+        this.nombre = nombre;
+    }
+
+    public Jugador(){
+        this.nombre = "Steve";
+    }
+}
