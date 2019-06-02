f798e530bdb204f4c887d8c1c5c9e5feaba5e159
lopezlin <morgan_pirata2003@yahoo.com.ar>
2019-06-01 23:46:10 -0300

    Creamos test01 para HachaDeMetal

---
+++ test/fiuba/algo3/Herramientas/HachaDeMetalTest.java
@@ -0,0 +1,26 @@
+package fiuba.algo3.Herramientas;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HachaDeMetalTest {
+
+/***********************************************/
+
+    @Test
+    public void test01CreamosInstanciaDeHachaDeMetal () {
+
+        HachaDeMetal unHachaDeMetal = new HachaDeMetal();
+        Assert.assertNotNull(unHachaDeMetal);
+
+    }
+
+/***********************************************/
+
+/***********************************************/
+
+/***********************************************/
+
+}
