47546d3fbf78d7a5b45f040745ad00fed559a357
Joaquin Andresen <joaquin_andresen@icloud.com>
2019-06-02 10:33:41 -0300

    Saco variables estaticas para que coincidan con las demas herramientas

--- src/fiuba/algo3/Herramientas/HachaDePiedra.java
+++ src/fiuba/algo3/Herramientas/HachaDePiedra.java
@@ -3,12 +3,9 @@ package fiuba.algo3.Herramientas;
 
 public class HachaDePiedra extends Herramienta {
 
-    static int DURABILIDAD_HACHA_PIEDRA = 200;
-    static int FUERZA_HACHA_PIEDRA      = 5;
-
     public HachaDePiedra ()
         {
-            this.durabilidad = DURABILIDAD_HACHA_PIEDRA;
-            this.fuerza = FUERZA_HACHA_PIEDRA;
+            this.durabilidad = 200;
+            this.fuerza = 5;
         }
 }
--- src/fiuba/algo3/Herramientas/PicoDePiedra.java
+++ src/fiuba/algo3/Herramientas/PicoDePiedra.java
@@ -1,13 +1,11 @@
 package fiuba.algo3.Herramientas;
 
 public class PicoDePiedra extends Herramienta {
-
-    static int DURABILIDAD_PICO_PIEDRA = 200;
-    static int FUERZA_PICO_PIEDRA      = 4;
+    
 
     public PicoDePiedra ()
         {
-            this.durabilidad = DURABILIDAD_PICO_PIEDRA;
-            this.fuerza = FUERZA_PICO_PIEDRA;
+            this.durabilidad = 200;
+            this.fuerza = 4;
         }
 }
