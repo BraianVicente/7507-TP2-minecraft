1d90534f564d9f1d0b1d3e22c950fbc039637de0
Joaquin Andresen <joaquin_andresen@icloud.com>
2019-06-02 17:09:10 -0300

    Agrego Inventario

--- src/fiuba/algo3/minecraft/jugador/Jugador.java
+++ src/fiuba/algo3/minecraft/jugador/Jugador.java
@@ -1,18 +1,27 @@
 package fiuba.algo3.minecraft.jugador;
 
+import fiuba.algo3.minecraft.herramienta.Herramienta;
+
 public class Jugador {
 
     public String nombre;
+    private Inventario inventario;
 
     public Jugador(String nombre){
         this.nombre = nombre;
+        this.inventario = new Inventario();
     }
 
     public Jugador(){
         this.nombre = "Steve";
+        this.inventario = new Inventario();
     }
 
     public String obtenerNombre(){
         return this.nombre;
+    }
+
+    public Herramienta obtenerHerramientaDeInventarioPorIndex(int index){
+        return this.inventario.obtenerHerramientaEnIndex(index);
     }
 }
