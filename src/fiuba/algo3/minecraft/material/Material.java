8678b7736ab04fa75546756da594187c0cad6629
lopezlin <morgan_pirata2003@yahoo.com.ar>
2019-06-02 11:41:15 -0300

    En la clase Materiales, se deja un solo metodo. Dado que todos son Herramientas no hace falta crear para cada herramienta un metodo desgaster (HachaDeMadera) desgaster (HachaDeMetal) desgastar (HachaDePiedra) desgastar (PicoDeMAdera) etc... con un desgastar (Herramienta unaHerramienta) es suficiente

--- src/fiuba/algo3/minecraft/material/Material.java
+++ src/fiuba/algo3/minecraft/material/Material.java
@@ -1,32 +1,16 @@
 package fiuba.algo3.minecraft.material;
 
-import fiuba.algo3.minecraft.herramienta.HachaDeMadera;
-import fiuba.algo3.minecraft.herramienta.HachaDeMetal;
-import fiuba.algo3.minecraft.herramienta.PicoDeMadera;
-import fiuba.algo3.minecraft.herramienta.PicoDeMetal;
+import fiuba.algo3.minecraft.herramienta.*;
 
 public abstract class Material {
 
     protected int durabilidad ;
 
-    public void desgastar(HachaDeMetal herramienta){
+    public void desgastar(Herramienta herramienta){
         int fuerzaHerramienta = herramienta.obtenerFuerza();
         durabilidad = durabilidad - fuerzaHerramienta ;
     }
 
-    public void desgastar(HachaDeMadera herramienta){
-        int fuerzaHerramienta = herramienta.obtenerFuerza();
-        durabilidad = durabilidad - fuerzaHerramienta ;
-    }
 
-    public void desgastar(PicoDeMetal herramienta){
-        int fuerzaHerramienta = herramienta.obtenerFuerza();
-        durabilidad = durabilidad - fuerzaHerramienta ;
-    }
-
-    public void desgastar(PicoDeMadera herramienta){
-        int fuerzaHerramienta = herramienta.obtenerFuerza();
-        durabilidad = durabilidad - fuerzaHerramienta ;
-    }
 
 }
