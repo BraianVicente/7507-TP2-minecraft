86ef8e6a71dbaa10c7c3a123362f845b532121b3
lopezlin <morgan_pirata2003@yahoo.com.ar>
2019-06-01 23:23:07 -0300

    Se crea clase HachaDePiedra

---
+++ src/fiuba/algo3/Herramientas/HachaDePiedra.java
@@ -0,0 +1,16 @@
+package fiuba.algo3.Herramientas;
+
+import com.sun.xml.internal.ws.api.pipe.ServerTubeAssemblerContext;
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+public class HachaDePiedra extends Herramienta {
+
+    static int DURABILIDAD_HACHA_PIEDRA = 200;
+    static int FUERZA_HACHA_PIEDRA      = 5;
+
+    public HachaDePiedra ()
+        {
+            this.durabilidad = DURABILIDAD_HACHA_PIEDRA;
+            this.fuerza = FUERZA_HACHA_PIEDRA;
+        }
+}
