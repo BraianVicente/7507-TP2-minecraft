ee7cf50b8ee7da1b5e0f466d9998736566bfe47f
lopezlin <morgan_pirata2003@yahoo.com.ar>
2019-06-05 13:55:36 -0300

    Empezamos a sacar test de Materiale y los ponemos en clases distintas: empezamos con DiamanteTest

---
+++ test/fiuba/algo3/minecraft/material/DiamanteTest.java
@@ -0,0 +1,29 @@
+package fiuba.algo3.minecraft.material;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DiamanteTest {
+
+/***********************************************/
+
+    @Test
+    public void test01CreamosInstanciaDeDiamante () {
+
+        Diamante unDiamante = new Diamante();
+        Assert.assertNotNull(unDiamante);
+    }
+
+/***********************************************/
+
+/***********************************************/
+
+/***********************************************/
+
+/***********************************************/
+
+/***********************************************/
+
+}
