2fc816ad8024ba54cd1c98a7b6b2ba205772af5d
Joaquin Andresen <joaquin_andresen@icloud.com>
2019-06-09 11:11:20 -0300

    Elimino ConstructorDeHacha y implemento todo en Constructor

--- src/fiuba/algo3/minecraft/modelo/constructor/Constructor.java
+++ src/fiuba/algo3/minecraft/modelo/constructor/Constructor.java
@@ -1,16 +1,46 @@
 package fiuba.algo3.minecraft.modelo.constructor;
 
-import fiuba.algo3.minecraft.modelo.herramienta.HachaDeMadera;
-import fiuba.algo3.minecraft.modelo.herramienta.HachaDePiedra;
-import fiuba.algo3.minecraft.modelo.herramienta.HachaDeMetal;
+import fiuba.algo3.minecraft.modelo.herramienta.*;
 import fiuba.algo3.minecraft.modelo.material.Madera;
 import fiuba.algo3.minecraft.modelo.material.Metal;
 import fiuba.algo3.minecraft.modelo.material.Piedra;
 
-public abstract class Constructor {
+public class Constructor {
 
-    public abstract HachaDeMadera construirHacha(Madera madera1, Madera madera2, Madera madera3, Madera madera4, Madera madera5);
-    public abstract HachaDePiedra construirHacha(Piedra piedra1, Piedra piedra2, Piedra piedra3, Piedra piedra4, Piedra piedra5);
-    public abstract HachaDeMetal construirHacha(Metal metal1, Metal metal2, Metal metal3, Metal metal4, Metal metal5);
+
+    public HachaDeMadera construirHacha(Madera madera1, Madera madera2, Madera madera3, Madera madera4, Madera madera5) {
+        HachaDeMadera hacha = new HachaDeMadera();
+        return hacha;
+    }
+
+    public HachaDePiedra construirHacha(Piedra piedra1, Piedra piedra2, Piedra piedra3, Piedra piedra4, Piedra piedra5) {
+        HachaDePiedra hacha = new HachaDePiedra();
+        return hacha;
+    }
+
+    public HachaDeMetal construirHacha(Metal metal1, Metal metal2, Metal metal3, Metal metal4, Metal metal5) {
+        HachaDeMetal hacha = new HachaDeMetal();
+        return hacha;
+    }
+
+    public PicoDeMadera construirPico(Madera madera1, Madera madera2, Madera madera3, Madera madera4, Madera madera5) {
+        PicoDeMadera pico = new PicoDeMadera();
+        return pico;
+    }
+
+    public PicoDePiedra construirPico(Piedra piedra1, Piedra piedra2, Piedra piedra3, Piedra piedra4, Piedra piedra5) {
+        PicoDePiedra pico = new PicoDePiedra();
+        return pico;
+    }
+
+    public PicoDeMetal construirPico(Metal metal1, Metal metal2, Metal metal3, Metal metal4, Metal metal5) {
+        PicoDeMetal pico = new PicoDeMetal();
+        return pico;
+    }
+
+    public PicoFino construirPico(Metal metal1, Metal metal2, Metal metal3, Piedra piedra1, Madera madera1, Madera madera2){
+        PicoFino pico = new PicoFino();
+        return pico;
+    }
 
 }
