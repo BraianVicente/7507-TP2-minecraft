6529dd1f094b01715f9cd7bb5d294e6a79a510b1
Joaquin Andresen <joaquin_andresen@icloud.com>
2019-06-10 09:23:20 -0300

    Elimino metodos que no sirven

--- src/fiuba/algo3/minecraft/modelo/constructor/Constructor.java
+++ src/fiuba/algo3/minecraft/modelo/constructor/Constructor.java
@@ -31,21 +31,6 @@ public class Constructor {
         return matriz[posicionEjeX][posicionEjeY];
     }
 
-    public HachaDeMadera construirHacha(Madera madera1, Madera madera2, Madera madera3, Madera madera4, Madera madera5) {
-        HachaDeMadera hacha = new HachaDeMadera();
-        return hacha;
-    }
-
-    public HachaDePiedra construirHacha(Piedra piedra1, Piedra piedra2, Piedra piedra3, Piedra piedra4, Piedra piedra5) {
-        HachaDePiedra hacha = new HachaDePiedra();
-        return hacha;
-    }
-
-    public HachaDeMetal construirHacha(Metal metal1, Metal metal2, Metal metal3, Metal metal4, Metal metal5) {
-        HachaDeMetal hacha = new HachaDeMetal();
-        return hacha;
-    }
-
     public Herramienta construirPicoDeMadera(Material [][] mesaDeTrabajo) {
         Material[][] distribucionPicoDeMadera = DistribucionPicoDeMadera();
         if (mesaDeTrabajo == distribucionPicoDeMadera) {
@@ -54,21 +39,6 @@ public class Constructor {
         } else {
             throw new RuntimeException("Se rompio todo");
         }
-    }
-
-    public PicoDePiedra construirPico(Piedra piedra1, Piedra piedra2, Piedra piedra3, Piedra piedra4, Piedra piedra5) {
-        PicoDePiedra pico = new PicoDePiedra();
-        return pico;
-    }
-
-    public PicoDeMetal construirPico(Metal metal1, Metal metal2, Metal metal3, Metal metal4, Metal metal5) {
-        PicoDeMetal pico = new PicoDeMetal();
-        return pico;
-    }
-
-    public PicoFino construirPico(Metal metal1, Metal metal2, Metal metal3, Piedra piedra1, Madera madera1, Madera madera2){
-        PicoFino pico = new PicoFino();
-        return pico;
     }
 
 }
