feb028289601a7e0aff6e463405bd60c09e7f5f4
FerFabbiano <fernando.fabbiano@gmail.com>
2019-06-10 15:10:09 -0300

    Agregor pruebas a Constructor.test que verifican que si se diseña mal una herramienta, no se va a poder construir

--- test/fiuba/algo3/minecraft/modelo/constructor/ConstructorTest.java
+++ test/fiuba/algo3/minecraft/modelo/constructor/ConstructorTest.java
@@ -17,7 +17,14 @@ public class ConstructorTest {
     }
 
     @Test
-    public void test02AgregarMaterialEnMesaDeTrabajo(){
+    public void test02CreamosInstanciaDeConstructorYLaMesaDeTrabajoSeInicializaVacia(){
+        Constructor constructor = new Constructor();
+
+        Assert.assertTrue(constructor.mesaDeTrabajoVacia());
+    }
+
+    @Test
+    public void test03AgregarMaterialEnMesaDeTrabajo(){
         Constructor constructor = new Constructor();
         Madera madera = new Madera();
 
@@ -28,7 +35,7 @@ public class ConstructorTest {
     }
 
     @Test
-    public void test03ConstruimosHachaDeMadera() {
+    public void test04ConstruimosHachaDeMadera() {
         Constructor constructor = new Constructor();
         Madera madera1 = new Madera();
         Madera madera2 = new Madera();
@@ -50,7 +57,7 @@ public class ConstructorTest {
     }
 
     @Test
-    public void test04ConstruimosHachaDeMetal() {
+    public void test05ConstruimosHachaDeMetal() {
         Constructor constructor = new Constructor();
         Metal metal1 = new Metal();
         Metal metal2 = new Metal();
@@ -72,7 +79,7 @@ public class ConstructorTest {
     }
 
     @Test
-    public void test05ConstruimosHachaDePiedra() {
+    public void test06ConstruimosHachaDePiedra() {
         Constructor constructor = new Constructor();
         Piedra piedra1 = new Piedra();
         Piedra piedra2 = new Piedra();
@@ -94,7 +101,7 @@ public class ConstructorTest {
     }
 
     @Test
-    public void test06ConstruimosPicoDeMadera() {
+    public void test07ConstruimosPicoDeMadera() {
         Constructor constructor = new Constructor();
         Madera madera1 = new Madera();
         Madera madera2 = new Madera();
@@ -116,7 +123,7 @@ public class ConstructorTest {
     }
 
     @Test
-    public void test07ConstruimosPicoDeMetal() {
+    public void test08ConstruimosPicoDeMetal() {
         Constructor constructor = new Constructor();
         Metal metal1 = new Metal();
         Metal metal2 = new Metal();
@@ -139,7 +146,7 @@ public class ConstructorTest {
     }
 
     @Test
-    public void test08ConstruimosPicoDePiedra() {
+    public void test09ConstruimosPicoDePiedra() {
         Constructor constructor = new Constructor();
         Piedra piedra1 = new Piedra();
         Piedra piedra2 = new Piedra();
@@ -162,7 +169,7 @@ public class ConstructorTest {
     }
 
     @Test
-    public void test09ConstruimosPicoFino() {
+    public void test10ConstruimosPicoFino() {
         Constructor constructor = new Constructor();
         Metal metal1 = new Metal();
         Metal metal2 = new Metal();
@@ -187,7 +194,7 @@ public class ConstructorTest {
     }
 
     @Test
-    public void test10MesaDeTrabajoSeReestableceDespuesDeConstrirHerramienta(){
+    public void test11MesaDeTrabajoSeReestableceDespuesDeConstrirHerramienta(){
         Constructor constructor = new Constructor();
         Madera madera1 = new Madera();
         Madera madera2 = new Madera();
@@ -204,6 +211,150 @@ public class ConstructorTest {
         Herramienta hachaDeMadera = constructor.construirHachaDeMadera();
 
         Assert.assertTrue(constructor.mesaDeTrabajoVacia());
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void test12DiseniamosMalElHachaDeMaderaYEntoncesNoSePuedeConstruir() {
+        Constructor constructor = new Constructor();
+        Madera madera1 = new Madera();
+        Madera madera2 = new Madera();
+        Madera madera3 = new Madera();
+        Madera madera4 = new Madera();
+        Madera madera5 = new Madera();
+
+        constructor.colocarMaterialEnMesaDeTrabajo(madera1, 0, 2);
+        constructor.colocarMaterialEnMesaDeTrabajo(madera2, 1, 0);
+        constructor.colocarMaterialEnMesaDeTrabajo(madera3, 0, 1);
+        constructor.colocarMaterialEnMesaDeTrabajo(madera4, 1, 1);
+        constructor.colocarMaterialEnMesaDeTrabajo(madera5, 1, 2);
+
+        HachaDeMadera hacha1 = new HachaDeMadera();
+        Herramienta hachaDeMadera = constructor.construirHachaDeMadera();
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void test13DiseniamosMalUnHAchaDeMetalYEntoncesNoSePuedeConstruir() {
+        Constructor constructor = new Constructor();
+        Metal metal1 = new Metal();
+        Metal metal2 = new Metal();
+        Metal metal3 = new Metal();
+        Madera madera1 = new Madera();
+        Madera madera2 = new Madera();
+
+        constructor.colocarMaterialEnMesaDeTrabajo(metal1, 0, 1);
+        constructor.colocarMaterialEnMesaDeTrabajo(metal2, 1, 0);
+        constructor.colocarMaterialEnMesaDeTrabajo(metal3, 0, 1);
+        constructor.colocarMaterialEnMesaDeTrabajo(madera1, 1, 1);
+        constructor.colocarMaterialEnMesaDeTrabajo(madera2, 1, 2);
+
+        HachaDeMetal hacha1 = new HachaDeMetal();
+        Herramienta hachaDeMetal = constructor.construirHachaDeMetal();
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void test14DiseniamosMalUnHachaDePiedraYEntoncesNoSePuedeConstruir() {
+        Constructor constructor = new Constructor();
+        Piedra piedra1 = new Piedra();
+        Piedra piedra2 = new Piedra();
+        Piedra piedra3 = new Piedra();
+        Madera madera1 = new Madera();
+        Madera madera2 = new Madera();
+
+        constructor.colocarMaterialEnMesaDeTrabajo(piedra1, 0, 2);
+        constructor.colocarMaterialEnMesaDeTrabajo(piedra2, 1, 0);
+        constructor.colocarMaterialEnMesaDeTrabajo(piedra3, 0, 1);
+        constructor.colocarMaterialEnMesaDeTrabajo(madera1, 1, 1);
+        constructor.colocarMaterialEnMesaDeTrabajo(madera2, 1, 2);
+
+        HachaDePiedra hacha1 = new HachaDePiedra();
+        Herramienta hachaDePiedra = constructor.construirHachaDePiedra();
+
+        Assert.assertEquals(hacha1.getClass(), hachaDePiedra.getClass());
+
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void test15DiseniamosMalPicoDeMaderaYEntoncesNoSePuedeConstruir() {
+        Constructor constructor = new Constructor();
+        Madera madera1 = new Madera();
+        Madera madera2 = new Madera();
+        Madera madera3 = new Madera();
+        Madera madera4 = new Madera();
+        Madera madera5 = new Madera();
+
+        constructor.colocarMaterialEnMesaDeTrabajo(madera1, 0, 2);
+        constructor.colocarMaterialEnMesaDeTrabajo(madera2, 1, 0);
+        constructor.colocarMaterialEnMesaDeTrabajo(madera3, 2, 0);
+        constructor.colocarMaterialEnMesaDeTrabajo(madera4, 1, 1);
+        constructor.colocarMaterialEnMesaDeTrabajo(madera5, 1, 2);
+
+        Herramienta picoDeMadera = constructor.construirPicoDeMadera();
+
+    }
+
+
+    @Test (expected = RuntimeException.class)
+    public void test16DiseniamosMalPicoDeMetalYEntoncesNoSePuedeConstruir() {
+        Constructor constructor = new Constructor();
+        Metal metal1 = new Metal();
+        Metal metal2 = new Metal();
+        Metal metal3 = new Metal();
+        Madera madera1 = new Madera();
+        Madera madera2 = new Madera();
+
+
+        constructor.colocarMaterialEnMesaDeTrabajo(metal1, 0, 2);
+        constructor.colocarMaterialEnMesaDeTrabajo(metal2, 1, 0);
+        constructor.colocarMaterialEnMesaDeTrabajo(metal3, 2, 0);
+        constructor.colocarMaterialEnMesaDeTrabajo(madera1, 1, 1);
+        constructor.colocarMaterialEnMesaDeTrabajo(madera2, 1, 2);
+
+        Herramienta picoDeMetal = constructor.construirPicoDeMetal();
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void test17DiseniamosMalPicoDePiedraYEntoncesNoSePuedeConstruir() {
+        Constructor constructor = new Constructor();
+        Piedra piedra1 = new Piedra();
+        Piedra piedra2 = new Piedra();
+        Piedra piedra3 = new Piedra();
+        Madera madera1 = new Madera();
+        Madera madera2 = new Madera();
+
+
+        constructor.colocarMaterialEnMesaDeTrabajo(piedra1, 0, 1);
+        constructor.colocarMaterialEnMesaDeTrabajo(piedra2, 1, 0);
+        constructor.colocarMaterialEnMesaDeTrabajo(piedra3, 2, 0);
+        constructor.colocarMaterialEnMesaDeTrabajo(madera1, 1, 1);
+        constructor.colocarMaterialEnMesaDeTrabajo(madera2, 1, 2);
+
+        Herramienta picoDePiedra = constructor.construirPicoDePiedra();
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void test18DisenamosMalPicoFinoYEntoncesNoSePuedeConstruir() {
+        Constructor constructor = new Constructor();
+        Metal metal1 = new Metal();
+        Metal metal2 = new Metal();
+        Metal metal3 = new Metal();
+        Piedra piedra1 = new Piedra();
+        Madera madera1 = new Madera();
+        Madera madera2 = new Madera();
+
+
+        constructor.colocarMaterialEnMesaDeTrabajo(metal1, 0, 2);
+        constructor.colocarMaterialEnMesaDeTrabajo(metal2, 1, 0);
+        constructor.colocarMaterialEnMesaDeTrabajo(metal3, 2, 0);
+        constructor.colocarMaterialEnMesaDeTrabajo(piedra1, 0, 1);
+        constructor.colocarMaterialEnMesaDeTrabajo(madera1, 1, 1);
+        constructor.colocarMaterialEnMesaDeTrabajo(madera2, 1, 2);
+
+        Herramienta picoFino = constructor.construirPicoFino();
 
     }
 
