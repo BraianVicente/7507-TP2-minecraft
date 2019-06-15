6e3a069eae98d31d8bfebc62f963ea5262c7a1cd
Joaquin Andresen <joaquin_andresen@icloud.com>
2019-06-15 16:14:57 -0300

    Agrego carpeta con imagen

--- src/fiuba/algo3/minecraft/modelo/vista/Vista.java
+++ src/fiuba/algo3/minecraft/modelo/vista/Vista.java
@@ -1,9 +1,12 @@
 package fiuba.algo3.minecraft.modelo.vista;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -19,13 +22,22 @@ public class Vista extends Application {
 
         stage.setTitle("AlgoCraft");
 
+        Image titulo = new Image("imagenes/titulo.jpg");
+
+        ImageView imageView = new ImageView(titulo);
+        imageView.setFitWidth(300);
+        imageView.setFitHeight(90);
+
         Button boton = new Button();
         boton.setText("Start");
 
         TextField texto = new TextField();
         texto.setPromptText("Ingrese el nombre del jugador");
+        texto.setMaxWidth(250);
 
-        VBox layoutPrincipal = new VBox(texto, boton);
+        VBox layoutPrincipal = new VBox(imageView, texto, boton);
+        layoutPrincipal.setSpacing(15);
+        layoutPrincipal.setAlignment(Pos.CENTER);
 
         Scene scene = new Scene(layoutPrincipal, 500, 300);
         stage.setScene(scene);
