05a4774f71053bc454ec71f171ca7cb826297e1e
Joaquin Andresen <joaquin_andresen@icloud.com>
2019-06-15 16:55:43 -0300

    Agrego background

--- src/fiuba/algo3/minecraft/modelo/vista/VistaInicial.java
+++ src/fiuba/algo3/minecraft/modelo/vista/VistaInicial.java
@@ -7,15 +7,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-public class Vista extends Application {
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+public class VistaInicial extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -23,10 +18,11 @@ public class Vista extends Application {
         stage.setTitle("AlgoCraft");
 
         Image titulo = new Image("imagenes/titulo.jpg");
+        Image background = new Image("imagenes/background.jpg");
 
-        ImageView imageView = new ImageView(titulo);
-        imageView.setFitWidth(300);
-        imageView.setFitHeight(90);
+        ImageView titulo1 = new ImageView(titulo);
+        titulo1.setFitWidth(300);
+        titulo1.setFitHeight(90);
 
         Button boton = new Button();
         boton.setText("Start");
@@ -35,12 +31,18 @@ public class Vista extends Application {
         texto.setPromptText("Ingrese el nombre del jugador");
         texto.setMaxWidth(250);
 
-        VBox layoutPrincipal = new VBox(imageView, texto, boton);
+        VBox layoutPrincipal = new VBox(titulo1, texto, boton);
         layoutPrincipal.setSpacing(15);
         layoutPrincipal.setAlignment(Pos.CENTER);
+        layoutPrincipal.setBackground(new Background(new BackgroundImage(background,
+                BackgroundRepeat.REPEAT,
+                BackgroundRepeat.REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT)));
 
         Scene scene = new Scene(layoutPrincipal, 500, 300);
         stage.setScene(scene);
+        stage.setResizable(false);
 
 
         stage.show();
---
+++ src/fiuba/algo3/minecraft/modelo/vista/VistaPrincipal.java
@@ -0,0 +1,17 @@
+package fiuba.algo3.minecraft.modelo.vista;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class VistaPrincipal extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
