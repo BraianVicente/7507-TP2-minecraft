029a92b47bc77f2c739825a5a6543866c2004430
Braian Vicente <bvicente@fi.uba.ar>
2019-06-16 21:03:46 -0300

    Prueba simple Movimiento

---
+++ src/fiuba/algo3/minecraft/modelo/tablero/Movimiento.java
@@ -0,0 +1,12 @@
+package fiuba.algo3.minecraft.modelo.tablero;
+
+public class Movimiento {
+
+    private Integer largo ;
+    private Integer alto ;
+
+    public Movimiento(Integer largo, Integer alto) {
+        this.alto = alto ;
+        this.largo = largo ;
+    }
+}
---
+++ test/fiuba/algo3/minecraft/modelo/tablero/MovimientoTest.java
@@ -0,0 +1,13 @@
+package fiuba.algo3.minecraft.modelo.tablero;
+
+
+import org.junit.Test;
+
+public class MovimientoTest {
+
+    @Test
+    public void testObtenerPosicionArribaIzquierda(){
+        Movimiento movimiento = new Movimiento(2,2);
+
+    }
+}
