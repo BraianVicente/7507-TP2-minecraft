27c4b6d879b9ee13cd7e0b470c253ca6c07e656c
FerFabbiano <fernando.fabbiano@gmail.com>
2019-06-17 16:26:52 -0300

    Agrego barra de menu

--- src/fiuba/algo3/minecraft/vista/JuegoVista.java
+++ src/fiuba/algo3/minecraft/vista/JuegoVista.java
@@ -2,6 +2,8 @@ package fiuba.algo3.minecraft.vista;
 
 import fiuba.algo3.minecraft.modelo.juego.Juego;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
@@ -29,13 +31,29 @@ public class JuegoVista {
 
         Juego juego = new Juego(nombreJugador);
 
+        BorderPane borderPane = new BorderPane();
+
+        MenuBar menuBar = new MenuBar();
+        Menu archivo = new Menu("Archivo");
+        MenuItem guardar = new MenuItem("Guardar");
+        MenuItem salir = new MenuItem("Salir");
+        Menu help = new Menu("Ayuda");
+        MenuItem acercaDe = new MenuItem("Acerca de..");
+        archivo.getItems().addAll(guardar, salir);
+        help.getItems().addAll(acercaDe);
+        menuBar.getMenus().addAll(archivo, help);
+
+        borderPane.setTop(menuBar);
+
         VBox mapa = matrizDeBotones.obtenerMapa();
 
         VBox contenedorDeControles = controles.obtenerControles();
 
         HBox contenedorHorizontal = new HBox(contenedorDeControles, mapa);
 
-        Scene escenaJuego = new Scene(contenedorHorizontal);
+        borderPane.setCenter(contenedorHorizontal);
+
+        Scene escenaJuego = new Scene(borderPane);
 
         this.escenario.setScene(escenaJuego);
     }
