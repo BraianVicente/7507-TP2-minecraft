c0f7f42ef9b21b499da39d40b34993d70672ffad
Joaquin Andresen <joaquin_andresen@icloud.com>
2019-06-17 15:13:54 -0300

    Agrego matriz de botones que diseño Fernando

--- src/fiuba/algo3/minecraft/vista/MapaVista.java
+++ src/fiuba/algo3/minecraft/vista/MapaVista.java
@@ -1,4 +1,21 @@
 package fiuba.algo3.minecraft.vista;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class MapaVista {
+    JButton[]arrBtn = new JButton[100];//arreglo de botones
+
+    private JPanel matrizDeBotones () {//constructor de la clase
+
+        JPanel jp1 = new JPanel(new GridLayout(10, 10, 0, 0));
+
+        for (int i = arrBtn.length - 1; i >= 0; i--) {//ciclo para crear, añadir, establecer propiedades a los botones
+            arrBtn[i] = new JButton("");
+            jp1.add(arrBtn[i]);
+            arrBtn[i].setMargin(new Insets(1, 1, 1, 1));
+        }//fin ciclo
+
+        return jp1;
+    }
 }
