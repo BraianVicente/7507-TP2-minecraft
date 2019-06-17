e46f3ee6b1a492f202ec0c2ed8525df871737723
FerFabbiano <fernando.fabbiano@gmail.com>
2019-06-17 19:26:46 -0300

    Arreglando error

--- src/fiuba/algo3/minecraft/vista/Controles.java
+++ src/fiuba/algo3/minecraft/vista/Controles.java
@@ -3,29 +3,16 @@ package fiuba.algo3.minecraft.vista;
 import fiuba.algo3.minecraft.modelo.juego.Juego;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 
 import java.awt.*;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 public class Controles {
 
     public VBox obtenerControles(Juego juego){
 
-<<<<<<< HEAD
-        Button flechaArriba = new Button("Arriba");
-
-        Button flechaAbajo = new Button("Abajo");
-
-        Button flechaIzquierda = new Button("Izquierda");
-
-        Button flechaDerecha = new Button("Derecha");
-=======
         MesaDeTrabajoVista mesaDeTrabajoVista = new MesaDeTrabajoVista(juego);
 
         VBox contenedorDeMesaDeTrabajo = mesaDeTrabajoVista.obtenerMesa();
@@ -52,7 +39,6 @@ public class Controles {
 
         Button flechaDerecha = new Button();
         flechaDerecha.setText("->");
->>>>>>> fac814faee19ff545687f52e76654310c02a8581
 
         HBox contenedorHorizontal = new HBox(flechaIzquierda, flechaAbajo, flechaDerecha);
         contenedorHorizontal.setSpacing(5);
