75bea538db072f35277e4d1736a9b73770f8dc07
Joaquin Andresen <joaquin_andresen@icloud.com>
2019-06-17 13:45:33 -0300

    Modifico comportamiento de Juego y muevo MesaDeTrabajoTest a carpeta nueva

--- src/fiuba/algo3/minecraft/modelo/juego/Juego.java
+++ src/fiuba/algo3/minecraft/modelo/juego/Juego.java
@@ -1,6 +1,7 @@
 package fiuba.algo3.minecraft.modelo.juego;
 
 import fiuba.algo3.minecraft.modelo.jugador.Jugador;
+import fiuba.algo3.minecraft.modelo.mapa.posicion.Posicion;
 import fiuba.algo3.minecraft.modelo.tablero.Tablero;
 
 public class Juego {
@@ -10,6 +11,9 @@ public class Juego {
     public Juego(String nombreJugador){
 
         this.tablero = new Tablero(new Jugador(nombreJugador));
+    }
 
+    public boolean moverJugadorA(int x, int y){
+        return this.tablero.mover(new Posicion(x,y));
     }
 }
