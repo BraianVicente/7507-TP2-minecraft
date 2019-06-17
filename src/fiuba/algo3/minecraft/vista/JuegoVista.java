15c03816188f957a91aaebff1eff3f8860e21e0c
FerFabbiano <fernando.fabbiano@gmail.com>
2019-06-17 17:17:02 -0300

    creo clase para la barra de menu

--- src/fiuba/algo3/minecraft/vista/JuegoVista.java
+++ src/fiuba/algo3/minecraft/vista/JuegoVista.java
@@ -3,6 +3,8 @@ package fiuba.algo3.minecraft.vista;
 import fiuba.algo3.minecraft.modelo.juego.Juego;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -18,6 +20,7 @@ public class JuegoVista {
     private Controles controles;
     private MapaVista matrizDeBotones;
     private Juego juego;
+    private BarraDeMenu menuBar;
 
 
     public JuegoVista(AlgoCraft aplicacion, Stage escenario){
@@ -25,6 +28,7 @@ public class JuegoVista {
         this.escenario = escenario;
         this.aplicacion = aplicacion;
         this.controles = new Controles();
+        this.menuBar = new BarraDeMenu();
     }
 
     public void iniciar(String nombreJugador){
@@ -34,24 +38,14 @@ public class JuegoVista {
         this.matrizDeBotones = new MapaVista(20,20, juego);
 
         BorderPane borderPane = new BorderPane();
-
-        MenuBar menuBar = new MenuBar();
-        Menu archivo = new Menu("Archivo");
-        MenuItem guardar = new MenuItem("Guardar");
-        MenuItem salir = new MenuItem("Salir");
-        Menu help = new Menu("Ayuda");
-        MenuItem acercaDe = new MenuItem("Acerca de..");
-        archivo.getItems().addAll(guardar, new SeparatorMenuItem(), salir);
-        help.getItems().addAll(acercaDe);
-        menuBar.getMenus().addAll(archivo, help);
-
-        borderPane.setTop(menuBar);
-
+        
         VBox mapa = matrizDeBotones.obtenerMapa();
 
         VBox contenedorDeControles = controles.obtenerControles();
 
         HBox contenedorHorizontal = new HBox(contenedorDeControles, mapa);
+
+        borderPane.setTop(menuBar.obtenerBarraDeMenu());
 
         borderPane.setCenter(contenedorHorizontal);
 
