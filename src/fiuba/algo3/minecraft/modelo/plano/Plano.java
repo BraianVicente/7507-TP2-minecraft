b78bffe81068b5e03ca5b8a59efd9ac3d6781438
FerFabbiano <fernando.fabbiano@gmail.com>
2019-06-19 01:24:32 -0300

    Agrego metodo para obtener elemento en una posicion del plano

--- src/fiuba/algo3/minecraft/modelo/plano/Plano.java
+++ src/fiuba/algo3/minecraft/modelo/plano/Plano.java
@@ -1,6 +1,8 @@
 package fiuba.algo3.minecraft.modelo.plano;
 
 import fiuba.algo3.minecraft.modelo.mapa.Mapa;
+import fiuba.algo3.minecraft.modelo.mapa.posicion.Posicion;
+import fiuba.algo3.minecraft.modelo.posicionable.Posicionable;
 
 public abstract class Plano {
 
@@ -8,6 +10,12 @@ public abstract class Plano {
 
     public Mapa obtenerPlano(){
         return plano;
+    }
+
+    public Posicionable obtenerElementoEnPosicion(Posicion posicion){
+        Posicionable elemento = plano.obtenerElementoEnPosicion(posicion);
+
+        return elemento;
     }
 
 }
